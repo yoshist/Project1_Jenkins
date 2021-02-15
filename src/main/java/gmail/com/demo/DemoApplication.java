fe6e1d2da3c8d2c808ccd0b0bfc41a7b4e870e89
Stacy Yoshimitsu <itsmest888@yahoo.com>
2021-02-15 07:07:47 +0000

    added changes

--- src/main/java/gmail/com/demo/DemoApplication.java
+++ src/main/java/gmail/com/demo/DemoApplication.java
@@ -5,6 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+  
+	public static double getRandomTicketNumber() {
+		double min=1000;
+		double max=9999;
+		double x = (int)(Math.random()*((max-min)+1))+min;
+		return x;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
--- src/test/java/gmail/com/demo/DemoApplicationTests.java
+++ src/test/java/gmail/com/demo/DemoApplicationTests.java
@@ -1,13 +1,17 @@
 package gmail.com.demo;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	private DemoApplication ticketer = new DemoApplication();
 	@Test
-	void contextLoads() {
+	void testRandoNumber() {
+
+	assertTrue(ticketer.getRandeomTicketNumber()>1000&ticketer.getRandomTicketNumber()<9999);
 	}
 
 }
